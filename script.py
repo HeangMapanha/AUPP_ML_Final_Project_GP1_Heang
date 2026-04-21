9d8c43f37957cd4ed0c6abf1b2cf4280c8c7da1b
Heang Mapanha <Mapanha.heang@gmail.com>
2026-04-21 11:24:08 +0700

    Data implentation

---
+++ script.py
@@ -0,0 +1,9 @@
+# something something
+
+
+# Dataset taken from Trashnet
+# Refine Dataset and Augment
+# Build ML model using CNN feature + Boost + Decision Tree + Random Forest + Linear Regression + Soft voting
+# Testing model
+# Build API for implementation
+# 
