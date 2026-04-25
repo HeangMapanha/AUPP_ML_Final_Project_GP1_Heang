760025c2a395841b1a3cddad971207462c73274c
Heang Mapanha <106654206+HeangMapanha@users.noreply.github.com>
2026-04-25 14:21:44 +0700

    html update

--- script.py
+++
@@ -1,9 +0,0 @@
-# something something
-
-
-# Dataset taken from Trashnet
-# Refine Dataset and Augment
-# Build ML model using CNN feature + Boost + Decision Tree + Random Forest + Linear Regression + Soft voting
-# Testing model
-# Build API for implementation
-# 
